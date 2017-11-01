f39944c5c8137632590e7c07dd5762eedc21d7d3
Gerald Leon Jones <gjones2@hydra13.eecs.utk.edu>
2017-11-01 12:23:54 -0400

    here ya go

---
+++ Singing-Skeleton.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+class Singing {
+	  public:
+		      int solve(int N, int low, int high, vector <int> pitch);
+};
+
+int Singing::solve(int N, int low, int high, vector <int> pitch)
+{
+	  int i;
+}
